bd33c3e72deaea587b52770cccd2860b6e1d9be2
Rodrigo Bosio <rodrigo.bosio@fatec.sp.gov.br>
2019-08-10 21:32:58 -0300

    Criação da classe de conexao

---
+++ Sistema Inicial Mod 01/SistemaHotel/SistemaHotel/Conexao.cs
@@ -0,0 +1,51 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaHotel
+{
+    class Conexao
+    {
+        //informaçõe necessarias para a conexao com o banco de dados(atualmente conectado localmente)
+        string conect = "SERVER=localhost; DATABASE=hotel; UID=root, PWD=; PORT=;";
+        public MySqlConnection con = null;
+
+        //metodo para abrir a conexao, ele necessita de um metodo para fecha-la
+        public void abrirConexao()
+        {
+            //tratamento de erros na conexao
+            try
+            {
+                con = new MySqlConnection(conect);
+                con.Open();
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+
+        }
+
+        //metodo para fechar a conexao
+        public void fecharConexao()
+        {
+            try
+            {
+                con = new MySqlConnection(conect);
+                con.Close();
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+
+        }
+
+
+    }
+}
